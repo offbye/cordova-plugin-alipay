c8c1492439e2a2bbbba00ba58c3e2851cbe6c91c
ZHU <haiyang.zhu@philips.com>
2017-05-23 12:30:38 +0800

    updated read me and sandbox support

--- www/AliPay.js
+++ www/AliPay.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.pay = function (paymentInfo, successCallback, errorCallback) {   
+exports.pay = function (paymentInfo, successCallback, errorCallback, isSandBox) {   
 		if(!paymentInfo){
 			errorCallback && errorCallback("Please enter order information");  
 		}else{
-			exec(successCallback, errorCallback, "AliPay", "pay", [paymentInfo]);
+			exec(successCallback, errorCallback, "AliPay", "pay", [paymentInfo, isSandBox || false]);
 		}
 };
